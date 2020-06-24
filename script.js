1f4364f444a95f1c32116e609758def93a143e93
Diachn <Diiianachen@gmail.com>
2020-06-23 21:11:23 -0700

    Created a JavaScript file

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $(".submit").click(function() {
+        var apiKey = "&appid=11283a9a367132ea8ba6c11df14c96c6";
+        var city = $(".inputText").val();
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + apiKey
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            var widget = show(response);
+            $("#show").html(widget);
+            $(".inputText").val("");
+            console.log(queryURL);
+
+
+            console.log(response);
+
+        })
+
+    });
+});
+
+function show(response) {
+    return "<p>City: " + response.name + "," + response.sys.country + "</p>" +
+        "<p>Description: " + response.weather[0].main + "</p>" +
+        "<p>Temperature: " + response.main.temp + "F" + "</p>" +
+        "<p>Wind Speed: " + response.wind.speed + "MPH" + "</p>" +
+        "<p>Humidity: " + response.main.humidity + "%" + "</p>"
+
+}
